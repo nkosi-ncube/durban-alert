80fd09b46cc872d8c5dc447d1fe8057512ce99fb
Nkosi Ncube <nkosi9976@gmail.com>
2025-07-28 17:22:32 +0000

    Error: `legacyBehavior` is deprecated and will be removed in a future re

--- src/components/dashboard-nav.tsx
+++ src/components/dashboard-nav.tsx
@@ -33,18 +33,16 @@ export function DashboardNav() {
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.href}>
-          <Link href={item.href} passHref legacyBehavior>
-            <SidebarMenuButton
-              asChild
-              isActive={pathname === item.href}
-              tooltip={{ children: item.label }}
-            >
-              <a>
-                <item.icon />
-                <span>{item.label}</span>
-              </a>
-            </SidebarMenuButton>
-          </Link>
+          <SidebarMenuButton
+            asChild
+            isActive={pathname === item.href}
+            tooltip={{ children: item.label }}
+          >
+            <Link href={item.href}>
+              <item.icon />
+              <span>{item.label}</span>
+            </Link>
+          </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>
